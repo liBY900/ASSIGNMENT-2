7830470dbf6fa0a5456bf9c6fb7d644c3fff31d2
liBY900 <ST10465691@RCCONNECT.EDU.ZA>
2025-04-25 09:04:03 +0100

    updated

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "IMAD ASSIGNMENT 2 APP"
+include(":app")
